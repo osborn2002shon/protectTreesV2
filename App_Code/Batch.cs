20793d395e17a20689aa22f7642b87053648a71b
Sean <sean@i-forcetech.com>
2026-01-07 19:08:17 +0800

    code sync

---
+++ App_Code/Batch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace protectTreesV2.Batch
+{
+    public class Batch
+    {
+        public enum enum_treeBatchType
+        {
+            Health,   // 健檢
+            Patrol,   // 巡查
+            Maintain  // 養護
+        }
+    }
+}
